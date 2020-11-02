1847f2222eca2dc2dba7cde8c0b67260b9a7b18d
Ankit Hans <ankithans1947@gmail.com>
2020-11-02 23:48:36 +0530

    feat: add register resolver

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,9 +1,29 @@
-import { Resolver, Mutation, Query } from "type-graphql";
+import { Resolver, Mutation, Query, Arg } from "type-graphql";
+import bcrypt from "bcryptjs";
+import { User } from "../../entity/User";
 
-@Resolver()
+@Resolver(User)
 export class RegisterResolver {
   @Query(() => String, { nullable: true })
   async hello() {
     return "Hello World";
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("firstName") firstName: string,
+    @Arg("lastName") lastName: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    }).save();
+
+    return user;
   }
 }
